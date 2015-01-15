c7d3fc09df7011907d397967236a7e0b8006af49
mist <mihail@sty-consulting.com>
2015-01-15 19:06:04 +0200

    inital commit: steve_example

---
+++ steve_example/steve/java/intro1/eclipse/Homework_2.java
@@ -0,0 +1,13 @@
+package steve.java.intro1.eclipse;
+
+/**
+ * Write a simple console app,
+ * that asks the user for their age,
+ * and then says “Your age is XX”
+ */
+public class Homework_2 {
+	
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ steve_example/steve/java/intro1/eclipse/Homework_3.java
@@ -0,0 +1,12 @@
+package steve.java.intro1.eclipse;
+
+
+/**
+ * Write the same program,
+ * but this time the age comes as a parameter
+ * and says the same thing
+ */
+public class Homework_3 {
+	public static void main(String[] args) {
+	}
+}
